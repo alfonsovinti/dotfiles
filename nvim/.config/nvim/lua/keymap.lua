2054f4a8b9a5119eef0264124bb3535d478b2b70
alfonsovinti <me@alfonsovinti.it>
2021-03-31 22:25:50 +0200

    edit

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -1,4 +1,5 @@
 local utils = require('utils')
+local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
 
 -- disable F1 bringing up the help doc every time
 utils.map('i', '<F1>', '<Nop>')
@@ -78,6 +79,28 @@ utils.map('n', 'gb', '<Cmd>bnext<CR>')
 utils.map('n', 'gB', '<Cmd>bprev<CR>')
 
 -- windows
+vim.g.tmux_navigator_no_mappings = 1
+utils.map('n', '<M-h>', ':TmuxNavigateLeft<cr>', {silent = true})
+utils.map('n', '<M-j>', ':TmuxNavigateDown<cr>', {silent = true})
+utils.map('n', '<M-k>', ':TmuxNavigateUp<cr>', {silent = true})
+utils.map('n', '<M-l>', ':TmuxNavigateRight<cr>', {silent = true})
+utils.map('n', '<M-\\>', ':TmuxNavigatePrevious<cr>', {silent = true})
+-- mooving windows
+utils.map('n', '<M-H>', '<C-w>H', {silent = true})
+utils.map('n', '<M-J>', '<C-w>J', {silent = true})
+utils.map('n', '<M-K>', '<C-w>K', {silent = true})
+utils.map('n', '<M-L>', '<C-w>L', {silent = true})
+-- resize windows
+utils.map('n', '<M-+>', '<C-w>+', {silent = true})
+utils.map('n', '<M-->', '<C-w>-', {silent = true})
+utils.map('n', '<M-<>', '<C-w><', {silent = true})
+utils.map('n', '<M->>', '<C-w>>', {silent = true})
+-- split current window horizzontally
+utils.map('n', '<M-_>', '<C-w>s', {silent = true})
+-- split current window vertically
+utils.map('n', '<M-|>', '<C-w>v', {silent = true})
+-- full screen
+vim.g.maximizer_default_mapping_key = '<M-f>'
 
 -- telescope
 utils.map('n', '<Leader>ff', '<Cmd>Telescope find_files<CR>')
@@ -85,3 +108,18 @@ utils.map('n', '<Leader>fg', '<Cmd>Telescope live_grep<CR>')
 utils.map('n', '<Leader>fb', '<Cmd>Telescope buffers<CR>')
 utils.map('n', '<Leader>fh', '<Cmd>Telescope help_tags<CR>')
 
+-- map j to gj and k to gk, so line navigation ignores line wrap
+-- ...but only if the count is undefined (otherwise, things like 4j
+-- break if wrapped LINES are present)
+cmd 'nnoremap <expr> j (v:count == 0 ? \'gj\' : \'j\')'
+cmd 'nnoremap <expr> k (v:count == 0 ? \'gk\' : \'k\')'
+cmd 'xnoremap <expr> j (v:count == 0 ? \'gj\' : \'j\')'
+cmd 'xnoremap <expr> k (v:count == 0 ? \'gk\' : \'k\')'
+
+-- TODO da modificare, sovrascrive altre keymap
+-- Opens an edit command with the path of the currently edited file filled in
+cmd 'noremap <Leader>e :ee <C-R>=expand("%:p:h") . "/" <CR>'
+-- Opens a tab edit command with the path of the currently edited file filled
+cmd 'noremap <Leader>et :tabe <C-R>=expand("%:p:h") . "/" <CR>'
+-- Opens a vertical split edit command with the path of the currently edited file filled
+cmd 'noremap <Leader>es :vsplit <C-R>=expand("%:p:h") . "/" <CR>'
