2e659dc35ce5893b61b0f1de5795a26b3bc37bd1
alfonsovinti <me@alfonsovinti.it>
2021-04-28 21:21:09 +0200

    add theme

--- fish/config.fish
+++ fish/config.fish
@@ -49,12 +49,32 @@ function fish_user_key_bindings
   fish_vi_key_bindings
 end
 
-# autocomplete and highlight
-set fish_color_normal brcyan
-set fish_color_autosuggestion '#7d7d7d'
-set fish_color_command brcyan
-set fish_color_error '#ff6c6b'
-set fish_color_param brcyan
+# set theme
+set fish_color_normal normal
+set fish_color_cancel -r
+set fish_color_history_current --bold
+set fish_pager_color_progress brwhite --background=cyan
+set fish_color_search_match bryellow --background=brblack
+set fish_color_selection white --bold --background=brblack
+set fish_color_param eceff4
+set fish_pager_color_prefix white --bold --underline
+set fish_pager_color_description B3A06D yellow
+set fish_pager_color_completion normal
+set fish_color_host normal
+set fish_color_user brgreen
+set fish_color_autosuggestion 4c566a
+set fish_color_valid_path --underline
+set fish_color_cwd_root red
+set fish_color_cwd green
+set fish_color_escape 00a6b2
+set fish_color_operator 00a6b2
+set fish_color_command 81a1c1
+set fish_color_quote a3be8c
+set fish_color_redirection b48ead
+set fish_color_end 88c0d0
+set fish_color_error ebcb8b
+set fish_color_comment 434c5e
+set fish_color_match --background=brblue
 
 # load aliases
 source "$DOTFILES/misc/aliases"
