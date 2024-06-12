e0366e0385ae74e530c1d442cf901d1cb7573e87
alfonsovinti <2808130+alfonsovinti@users.noreply.github.com>
2024-06-12 14:13:46 +0200

    add pcmanfm and kitty desktop integration

--- scripts/debian/add-de.sh
+++ scripts/debian/add-de.sh
@@ -35,9 +35,16 @@ sudo systemctl enable acpid
 sudo apt install -y lxpolkit network-manager network-manager-gnome 
 
 # File explorer
+sudo apt install -y pcmanfm
 
 # Terminal
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+echo 'kitty.desktop' > ~/.config/xdg-terminals.list
 
 # Sound
 sudo apt install -y pipewire pavucontrol pamixer
