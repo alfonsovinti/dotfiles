e84dfeec978f9e0a7042a37e0d1ee3e2f3189f01
alfonsovinti <2808130+alfonsovinti@users.noreply.github.com>
2024-06-12 22:47:29 +0200

    add polybar and openbox config

--- scripts/debian/link-config.sh
+++ scripts/debian/link-config.sh
@@ -1,8 +1,13 @@
 
 mkdir -p $HOME/.config
 mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/polybar
+mkdir -p $HOME/.config/openbox
 
 ln -sf $CONFIG_DIR/shared/starship/starship.toml $HOME/.config/starship.toml
 ln -sf $CONFIG_DIR/shared/tmux/tmux.conf $HOME/.tmux.conf
+
 ln -sf $CONFIG_DIR/debian/bash/bashrc $HOME/.bashrc
 ln -sf $CONFIG_DIR/debian/misc/inputrc $HOME/.inputrc
+ln -sf $CONFIG_DIR/debian/polybar/config.ini $HOME/.config/polybar/config.ini
+ln -sf $CONFIG_DIR/debian/openbox/rc.xml $HOME/.config/openbox/rc.xml
