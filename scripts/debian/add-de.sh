0826479ec4e0551d3e389a10b4e121ff58e27766
alfonsovinti <2808130+alfonsovinti@users.noreply.github.com>
2024-06-13 15:40:14 +0200

    improved

--- scripts/debian/add-de.sh
+++ scripts/debian/add-de.sh
@@ -39,16 +39,7 @@ sudo apt install -y lxpolkit network-manager network-manager-gnome
 sudo apt install -y pcmanfm
 
 # Openbox packages
-sudo apt install -y openbox lxrandr lxappearance-obconf lxinput
-
-# Terminal
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+sudo apt install -y openbox lxrandr lxappearance-obconf lxinput lxterminal
 
 # Sound
 sudo apt install -y pipewire pavucontrol pamixer
---
+++ scripts/debian/add-input-synaptics.sh
@@ -0,0 +1,3 @@
+# https://wiki.debian.org/it/SynapticsTouchpad
+
+sudo apt install -y xserver-xorg-input-synaptics
