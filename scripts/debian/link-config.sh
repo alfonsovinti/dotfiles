7086e71289bd1c5b8f5ddba2f0bd32149a6d9e6a
alfonsovinti <2808130+alfonsovinti@users.noreply.github.com>
2024-06-17 13:49:29 +0200

    fix openbox exit menu

--- scripts/debian/link-config.sh
+++ scripts/debian/link-config.sh
@@ -18,6 +18,7 @@ ln -sf $CONFIG_DIR/debian/polybar/themes/mocha.ini $HOME/.config/polybar/theme.i
 # openbox
 ln -sf $CONFIG_DIR/debian/openbox/rc.xml $HOME/.config/openbox/rc.xml
 ln -sf $CONFIG_DIR/debian/openbox/menu.xml $HOME/.config/openbox/menu.xml
+ln -sf $CONFIG_DIR/debian/openbox/menu.systemd.xml $HOME/.config/openbox/menu.systemd.xml
 ln -sf $CONFIG_DIR/debian/openbox/environment $HOME/.config/openbox/environment
 ln -sf $CONFIG_DIR/debian/openbox/autostart $HOME/.config/openbox/autostart
 ln -sf $CONFIG_DIR/debian/conky/mocha.conf $HOME/.config/conky/conky.conf
